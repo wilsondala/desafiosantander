e964321408f5a4b7b7c486dfe5420a98b9022a3c
wilsondala <dalawilson1244gmail.com>
2025-02-18 14:33:25 -0300

    atualizacao

---
+++ .github/workflows/server.js
@@ -0,0 +1,14 @@
+// server.js
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello, CI/CD!');
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
+
+module.exports = app;
